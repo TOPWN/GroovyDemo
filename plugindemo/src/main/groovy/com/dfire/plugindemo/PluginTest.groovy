cf7bbb6bf75d37d0ac7d72f681e80e45cf1add26
TOPWN <danggui@2dfire.com>
2018-05-13 14:36:56 +0800

    add plugin

--- plugindemo/src/main/groovy/com/dfire/plugindemo/PluginTest.groovy
+++ plugindemo/src/main/groovy/com/dfire/plugindemo/PluginTest.groovy
@@ -3,7 +3,7 @@ package com.dfire.plugindemo
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-public class PluginDemo implements Plugin<Project> {
+public class PluginTest implements Plugin<Project> {
     void apply(Project project) {
         project.task('testTask') << {
             println "Hello gradle plugin"
