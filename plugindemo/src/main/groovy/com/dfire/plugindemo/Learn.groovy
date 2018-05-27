a3c09b40c0fd8f9b7d43b9d51cbc7e01d095f468
TOPWN <danggui@2dfire.com>
2018-05-27 14:23:02 +0800

    update

--- plugindemo/src/main/groovy/com/dfire/plugindemo/Learn.groovy
+++ plugindemo/src/main/groovy/com/dfire/plugindemo/Learn.groovy
@@ -30,7 +30,7 @@ numbers[1] = 0
 assert letters[1] == 0
 //新增值
 numbers << 4
-
+numbers.each {}
 //类型强转
 def linkedList = [2, 3, 4] as LinkedList
 
@@ -83,6 +83,9 @@ def foo(String par1, Integer par2 = 1) {
 
 assert foo('Marie').age == 1
 
+//省略括号
+foo "hello", 20
+
 /**
  * Class
  */
@@ -98,31 +101,58 @@ def p = new Person()
 p.name
 
 /**
- * Closure(闭包)
+ * Closure(闭包)，不同于JS中的闭包
  */
-def item = 1
-def c0 = { item++ }
 
-def c1 = { -> item++ }
-
-def c2 = { println it }
-
-def c3 = { it -> println it }
-
-def c4 = { age -> println age } //Groovy的闭包可以引用外部变量
-
-def c5 = { String x, int y ->
+def c0 = { String x, int y ->
     println "hey ${x} the value is ${y}"
 }
+
+def c1 = { age -> println age }
+
+def c2 = { it -> println it }
+
+def c3 = { println it }
+
+def item = 1
+
+def c4 = { -> item++ }
+
+def c5 = { item++ }
 
 def listener = { e -> println "Clicked on $e" }
 assert listener instanceof Closure
 
-Closure callback = { println 'Done!' }
-
 Closure<Boolean> isTextFile = {
     File it -> it.name.endsWith('.txt')
 }
-//调用闭包
-def result = isTextFile(new File())
-def callResult = isTextFile.call(new File())
+//调用闭包,最后一行作为返回值
+def result = isTextFile(new File('xx/yy/z.txt'))
+def callResult = isTextFile.call(new File('xx/yy/z.txt'))
+
+//最后一行无返回值，则返回null
+Closure callback = { println 'Done!' }
+
+def greeting = { "Hello, $it!" }
+assert greeting('Patrick') == 'Hello, Patrick!'
+
+//可以省略圆括号
+greeting 'Patrick'
+
+//将closure作为method参数
+def lst = [11, 12, 13, 14]
+lst.each { println it }//print "11 12 13 14"
+
+//如果函数的最后一个参数是闭包的话，可以省略圆括号
+def testClosure(int a1, String b1, Closure closure) {
+    //do something
+}
+
+//当闭包作为闭包或方法的最后一个参数。可以将闭包从参数圆括号中提取出来接在最后，如果闭包是唯一的一个参数，则闭包或方法参数所在的圆括号也可以省略
+testClosure(4, "test") {
+    println "i am in closure"
+}
+
+testClosure 4, "hello", {
+    println "i am in closure"
+}
